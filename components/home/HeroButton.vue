3622f19dadd439118f4a20e05d12637c2fefce59
Duale Siad <me@wale.id.au>
2024-03-01 17:35:08 +1100

    fix(frontend): use correct prop syntax in `HeroButton`s

--- components/home/HeroButton.vue
+++ components/home/HeroButton.vue
@@ -2,7 +2,7 @@
     <div
         class="rounded-lg px-4 py-2 md:px-6 md:py-4 bg-grayscale-600 text-grayscale-200"
     >
-        <a href="{{ href }}">{{ name }}</a>
+        <a :href="href">{{ name }}</a>
     </div>
 </template>
 
